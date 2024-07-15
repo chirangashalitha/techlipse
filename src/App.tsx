446fd6a1a37fae0b3aa941c140ccad54ab4e2f49
chirangashalitha <chiranga.20244008@iit.ac.lk>
2024-07-15 12:42:01 +0530

    Create react project

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import ItemSlider from "./components/ItemSlider/ItemSlider";
+import Navbar from "./components/Navbar/Navbar";
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <ItemSlider />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/ItemCard/ItemCard.tsx
@@ -0,0 +1,38 @@
+import { useState } from "react";
+import AddToCartButton from "./AddToCartButton";
+import Alert from "../Alert";
+import './ItemCard.css';
+
+interface Props {
+  title: string;
+  price?: string;
+  src: string;
+}
+
+function ItemCard({ title, price, src }: Props) {
+  const [alertVisible, setAlertVisibility] = useState(false);
+
+  return (
+    <>
+      <div>
+        {alertVisible && (
+          <Alert onClose={() => setAlertVisibility(false)}>
+            Successfully Added to Cart!
+          </Alert>
+        )}
+      </div>
+      <div className="card" style={{ width: "18rem" }}>
+        <img src={src} className="card-img-top item-img" alt="..." />
+        <div className="card-body">
+          <h5 className="card-title">{title}</h5>
+          <p className="card-text">{price}</p>
+          <AddToCartButton onClick={() => setAlertVisibility(true)}>
+            Add to Cart
+          </AddToCartButton>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default ItemCard;
